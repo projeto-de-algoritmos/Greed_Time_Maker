a75937019117caaa35273ce1cfb7989915bee53a
PedroMoraes39 <phcmoraes39@gmail.com>
2023-10-30 23:19:41 -0300

    Arruando criação de salas

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ def criar_sala(nome_sala, capacidade, horarios_disponiveis):
     nova_sala = SalaDeReuniao(nome_sala, horarios_disponiveis, capacidade)
     salas.append(nova_sala)
 
+def agendar_reuniao(nova_reuniao):
+    for sala in salas:
+        if sala.horarios_disponiveis[0] <= nova_reuniao.inicio <= sala.horarios_disponiveis[1]:
+            if not any(reuniao.inicio <= nova_reuniao.inicio <= reuniao.fim for reuniao in sala.reunioes_agendadas):
+                sala.agendar_reuniao(nova_reuniao)
+                return True
+    return False
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
@@ -20,29 +28,14 @@ def index():
 
         nova_reuniao = Reuniao(nome_reuniao, inicio_reuniao, fim_reuniao, data_reuniao)
 
-        sala_existente = False
-        for sala in salas:
-            if sala.horarios_disponiveis[0] <= nova_reuniao.inicio <= sala.horarios_disponiveis[1] and len(sala.reunioes_agendadas) < sala.capacidade:
-                sala.agendar_reuniao(nova_reuniao)
-                sala_existente = True
-                break
-
-        if not sala_existente:
-            for sala in salas:
-                if len(sala.reunioes_agendadas) < sala.capacidade and not any(reuniao.inicio <= nova_reuniao.inicio <= reuniao.fim for reuniao in sala.reunioes_agendadas):
-                    sala.agendar_reuniao(nova_reuniao)
-                    sala_existente = True
-                    break
-
-        if not sala_existente:
+        if not agendar_reuniao(nova_reuniao):
             nome_sala = f"Sala {len(salas) + 1}"
             capacidade = 10
-            horarios_disponiveis = [8, 17]  
+            horarios_disponiveis = [8, 17]
             criar_sala(nome_sala, capacidade, horarios_disponiveis)
-            salas[-1].agendar_reuniao(nova_reuniao)
+            agendar_reuniao(nova_reuniao)
 
     return render_template("index.html", salas=salas)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
