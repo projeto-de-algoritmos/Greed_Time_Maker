b00fbb5fff86d851b98344406b559ef1de606a55
SamuelCastro7 <samuelcastro2010@gmail.com>
2023-10-30 22:17:28 -0300

    Melhorando a interface

--- reunioes/reuniao.py
+++ reunioes/reuniao.py
@@ -1,8 +1,10 @@
 class Reuniao:
-    def __init__(self, nome, inicio, fim):
+    def __init__(self, nome, inicio, fim, data, sala=None):
         self.nome = nome
         self.inicio = inicio
         self.fim = fim
+        self.data = data
+        self.sala = sala
 
     def __str__(self):
         return f"Reunião '{self.nome}' das {self.inicio}h às {self.fim}h"
